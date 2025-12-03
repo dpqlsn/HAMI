567b88488402253ee96ee81c2fab18afcc0aaaf3
dpqlsn <a01037111209@gmail.com>
2025-12-03 14:56:19 +0900

    feat :: 3초 뒤 자동으로 이동

--- src/container/home/index.tsx
+++ src/container/home/index.tsx
@@ -1,10 +1,22 @@
 "use client";
 
 import * as S from './style';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
+    const router = useRouter();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            router.push('/');
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }, [router]);
+
     return (
-        <S.Contaienr>
+        <S.Contaienr onClick={() => router.push('/')}>
             <S.TextGroup>
                 <S.Title>당신의 아이디어를</S.Title>
                 <S.Title>시험해보세요</S.Title>
